183a27e73c3263826a57830b38f61ccdfbbb8367
Christopher Cid <iamcid17@gmail.com>
2020-03-14 00:30:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {railroads: {pieces: => 4, rent_in_dollars: => {one_piece_owed: => 25, two_pieces_owed: => 50, three_pieces_owed: => 100, four_pieces_owed: => 200 }}}
+	monopoly = {railroads: {pieces:=> 4, rent_in_dollars: => {one_piece_owed: => 25, two_pieces_owed: => 50, three_pieces_owed: => 100, four_pieces_owed: => 200 }}}
 
 end
 
